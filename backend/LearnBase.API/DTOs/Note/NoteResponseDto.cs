ddf0e52f8be42d0fd50c894af1da5c228c6e5b1d
unknown <youssefnouira20@gmail.com>
2026-04-19 23:11:09 +0100

    finished note DTOs

---
+++ backend/LearnBase.API/DTOs/Note/NoteResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace LearnBase.API.DTOs.Note
+{
+    public class NoteResponseDto
+    {
+        public Guid NoteId { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ backend/LearnBase.API/DTOs/Note/UpdateNoteDto.cs
@@ -0,0 +1,7 @@
+﻿namespace LearnBase.API.DTOs.Note
+{
+    public class UpdateNoteDto
+    {
+        public string Content { get; set; } = string.Empty;
+    }
+}
