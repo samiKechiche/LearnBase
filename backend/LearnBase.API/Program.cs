872f5cd05ed1e22c0f6abc0d1a53c56424a1b3bc
unknown <youssefnouira20@gmail.com>
2026-04-29 21:25:15 +0100

    small change for lesson service

--- backend/LearnBase.API/Program.cs
+++ backend/LearnBase.API/Program.cs
@@ -25,6 +25,7 @@ builder.Services.AddScoped<PracticeSessionStatsService>();
 builder.Services.AddScoped<JwtService>();
 builder.Services.AddScoped<PasswordHasherService>();
 builder.Services.AddScoped<AuthService>();
+builder.Services.AddScoped<LessonService>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
