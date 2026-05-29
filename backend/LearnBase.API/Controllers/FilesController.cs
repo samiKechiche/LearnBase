4165961f72623c316c660383f1c29463e9870974
unknown <youssefnouira20@gmail.com>
2026-05-29 17:21:41 +0100

    added file upload logic

---
+++ backend/LearnBase.API/Controllers/FilesController.cs
@@ -0,0 +1,32 @@
+﻿using LearnBase.API.DTOs.File;
+using LearnBase.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearnBase.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FilesController : ControllerBase
+    {
+        private readonly FileService _fileService;
+
+        public FilesController(FileService fileService)
+        {
+            _fileService = fileService;
+        }
+
+        [HttpPost("upload")]
+        public async Task<IActionResult> Upload([FromForm] UploadFileDto dto)
+        {
+            try
+            {
+                var result = await _fileService.UploadFileAsync(dto);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
