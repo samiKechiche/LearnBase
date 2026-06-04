d9e934b3b2b41e0a3a46c13d4636be9555c04a59
unknown <youssefnouira20@gmail.com>
2026-06-04 12:09:46 +0100

    bug fix

--- backend/LearnBase.API/DTOs/FileUploadDto.cs
+++
@@ -1,8 +0,0 @@
-namespace LearnBase.API.DTOs
-{
-    // Simple form model so Swagger can generate multipart/form-data schema
-    public class FileUploadDto
-    {
-        public IFormFile? File { get; set; }
-    }
-}
